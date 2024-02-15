19b1901e3c58d19bb0e7074a196c6fbcac6691e2
tavolafourcade <tavolafourcade@gmail.com>
2024-02-15 14:52:27 -0500

    [ ZAIC-9 ] UPDATE source images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,16 +21,19 @@ const Home = () => {
       <div>
         <CarouselComponent />
 
-        <div className='absolute ml-6 mr-12 top-32 lg:top-60 lg:w-[45rem] lg:ml-[10rem] lg:h-[38rem] flex flex-col justify-between'>
+        <div className='absolute ml-6 mr-12 top-36 lg:top-60 lg:w-[45rem] lg:ml-[10rem] lg:h-[38rem] flex flex-col justify-between'>
           <div className='  text-white flex flex-col'>
-            <span className='text-sm lg:text-2xl'>{launchDate}</span>
-            <span className='text-3xl lg:text-[3.125rem] font-bold lg:leading-[3.5rem] pt-4 pb-5'>{title}</span>
-            <span className='text-base lg:text-2xl font-normal'>{subTitle}</span>
+            <span className='text-sm lg:text-2xl font-onest font-thin'>{launchDate}</span>
+            <span className='text-3xl lg:text-[3.125rem] font-bold lg:leading-[3.5rem] pt-6 pb-5 lg:pt-4 font-onest'>{title}</span>
+            <span className='text-base lg:text-2xl font-normal font-onest'>{subTitle}</span>
           </div>
-          <div className='text-white lg:text-xl'>
+          <div className='text-white lg:text-xl hidden lg:block font-onest'>
             <span>{contact} <strong>{email}</strong></span>
           </div>
         </div>
+      </div>
+      <div className='text-primary bg-light max-md:text-xl lg:hidden h-[20vh] flex p-4 items-center font-onest'>
+        <span className='text-base'>{contact} <strong>{email}</strong></span>
       </div>
     </div>
   )
