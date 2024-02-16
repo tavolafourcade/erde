f02e119d75da5623ab4857be2606e2c5f9a1b8b8
tavolafourcade <tavolafourcade@gmail.com>
2024-02-16 17:57:13 -0500

    [ ZAIC-9 ] DELETE import commented

--- src/app/components/Carrousel.tsx
+++ src/app/components/Carrousel.tsx
@@ -3,7 +3,6 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Arrow from '../../../public/images/Arrow'
-// import Background from '../../../public/images/Background'
 import Image from 'next/image'
 
 // Componentes de flecha
