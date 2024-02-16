f1389e7f25e8a853ea7d5065095bbde8575b1bd4
tavolafourcade <tavolafourcade@gmail.com>
2024-02-16 17:57:35 -0500

    [ ZAIC-9 ] UPDATE Fontsize for medium size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,9 +23,9 @@ const Home = () => {
 
         <div className='absolute ml-6 mr-12 top-36 lg:top-56 lg:w-[45rem] lg:ml-[10rem] lg:h-[36rem] flex flex-col justify-between'>
           <div className='  text-white flex flex-col'>
-            <span className='text-sm lg:text-2xl font-onest font-thin'>{launchDate}</span>
-            <span className='text-3xl lg:text-[3.125rem] font-bold lg:leading-[3.5rem] pt-6 pb-5 lg:pt-4 font-onest'>{title}</span>
-            <span className='text-base lg:text-2xl font-normal font-onest'>{subTitle}</span>
+            <span className='text-sm lg:text-2xl font-onest font-thin md:text-xl'>{launchDate}</span>
+            <span className='text-3xl md:text-4xl lg:text-[3.125rem] font-bold lg:leading-[3.5rem] pt-6 pb-5 lg:pt-4 font-onest'>{title}</span>
+            <span className='text-base md:text-xl lg:text-2xl font-normal font-onest'>{subTitle}</span>
           </div>
           <div className='text-white lg:text-xl hidden lg:block font-onest'>
             <span>{contact} <strong>{email}</strong></span>
